5da8a1a21b1c0b9b0364a86a0cd1fccff7bf54de
Jaid <jaid.jsx@gmail.com>
2019-07-06 11:14:25 +0200

    Added higher timeout to test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import path from "path"
 
 import coffee from "coffee"
+import ms from "ms.macro"
 
 const main = path.resolve(process.env.MAIN)
 
@@ -8,4 +9,4 @@ it("should run", () => coffee.fork(main, ["_something-that-does-not-exist_"])
   .expect("code", 0)
   .expect("stdout", /No repository named _something-that-does-not-exist_ found\./)
   .debug(true)
-  .end())
+  .end(), ms`1 minute`)
