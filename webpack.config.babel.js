08adcace74cf007d5e871ec7449c19e8b3744ae9
Jaid <jaid.jsx@gmail.com>
2019-07-06 10:52:57 +0200

    improve: Set binary name to `open-project` and also `open-project-cli`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,6 +3,6 @@ import configure from "webpack-config-jaid"
 export default configure({
   publishimo: {
     fetchGithub: true,
-    binNames: "open-project",
+    binName: "open-project",
   },
 })
